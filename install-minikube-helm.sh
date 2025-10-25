0d7bf1b6bf4f2c84dd9907957d3049da19b10be2
Muhammad Fadly <mfadly24@gmail.com>
2025-10-25 09:58:51 +0700

    update install minikube

--- install-minikube-helm.sh
+++ install-minikube-helm.sh
@@ -13,11 +13,32 @@ echo 'Enable Addon Ingress on Minikube'
 minikube addons enable ingress
 
 sleep 1
-echo 'Installing Helm V2...'
+echo 'Installing Helm v2.17 ...'
 if [ ! -f ./helm-v2.17.0-linux-amd64.tar.gz ]; then
     curl -LO https://get.helm.sh/helm-v2.17.0-linux-amd64.tar.gz
 fi
 tar -zxvf helm-v2.17.0-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/helmv2
 helmv2 version
-echo 'Helm v2 Installed'
+echo 'Helm v2.17 Installed'
+
+sleep 1
+echo 'Installing Tiller for Helm v2.17...'
+minikube kubectl -- -n kube-system create serviceaccount tiller
+minikube kubectl -- create clusterrolebinding tiller \
+  --clusterrole cluster-admin \
+  --serviceaccount=kube-system:tiller
+helmv2 init --service-account tiller
+helmv2 version
+echo 'Tiller for Helm v2.17 Installed'
+
+sleep 1
+echo 'Installing Helm v2.14 client for Supported Version On Premise...'
+if [ ! -f ./helm-v2.14.0-linux-amd64.tar.gz ]; then
+    curl -LO https://get.helm.sh/helm-v2.14.0-linux-amd64.tar.gz
+fi
+tar -zxvf helm-v2.14.0-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helmv2
+helmv2 version
+echo 'Helm v2.14 Installed'
+
